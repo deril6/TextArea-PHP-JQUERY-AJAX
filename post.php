6a5f01c7e18e5504b00cc7ecdf92b8b3f87dc690
Manny See Te <emmanuelsalaum@gmail.com>
2016-02-28 14:42:46 +0800

    Update post.php

--- post.php
+++ post.php
@@ -2,10 +2,9 @@
 require_once('conn.php');
 if(isset($_POST['post'])) {
     $content = $_POST['post'];
-    $content = str_ireplace("<br><br>","",$content);
+    $content = preg_replace('/(<br\ ?\/?>)+/','<br>',$content);
     $detect_url = '#(https?|ftp|file)://[-A-Za-z0-9+&@\#/%()?=~_|$!:,.;]*[-A-Za-z0-9+&@\#/%()=~_|$]#';
     if($content != "") {
-//        echo count(preg_match($detect_url,$content,$url));
         if(preg_match($detect_url,$content,$url)) {
             $content = '<div class="view">'.preg_replace($detect_url,'<a target="_blank" href="'.$url[0].'">'.$url[0].'</a>',$content).'</div>';
         } else {
